b25885d33a01b61510138d7c479a0c1a4325b4ca
David Jones <coreldavid@hotmail.com>
2020-07-15 18:40:15 -0400

    Added ability to get all profiles and by ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,12 +8,12 @@ const { User } = require('../../models/User');
 
 // @route  Get api/profile
 // @desc   Test route
-// @acess  Public
+// @access  Public
 //router.get('/', (req,res) => res.send('Profile Route'));
 
 // @route  Get api/profile/me
 // @desc   Get current users profile
-// @acess  Private
+// @access  Private
 router.get('/me', auth, async (req,res) => {
   try{
     const profile = await Profile.findOne({ user: req.user.id }).populate('user',
@@ -110,6 +110,40 @@ router.post(
       console.error(err.message);
       res.status(500).send('Server error');
     }
+});
+
+// @route  Get api/profile
+// @desc   Get all profile
+// @access  Public
+router.get('/', async (req,res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  Get api/profile/user/:user_id
+// @desc   Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req,res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+    
+    if(!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind == 'ObjectId'){
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
