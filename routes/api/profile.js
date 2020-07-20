693e6227ca7f03eda611a3429e095a87278f1b6b
David Jones <coreldavid@hotmail.com>
2020-07-20 14:43:50 -0400

    Created route to add experience to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -173,48 +173,51 @@ router.put('/experience', [ auth, [
   check('title', 'Title is required')
     .not()
     .isEmpty(),
-    check('company', 'Company is required')
-      .not()
-      .isEmpty(),
-    check('from', 'From date is required')
-      .not()
-      .isEmpty()
-]], async (req,res) => {
-      const errors = validationResult(req);
-      if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array() });
-      }
+  check('company', 'Company is required')
+    .not()
+    .isEmpty(),
+  check('from', 'From date is required')
+    .not()
+    .isEmpty()
+]], 
+  async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-      const {
-        title,
-        company,
-        location,
-        from,
-        to,
-        current,
-        description
-      } = req.body;
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
       
-      const newExo = {
-        title,
-        company,
-        location,
-        from,
-        to,
-        current,
-        description
-      }
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    }
 
-      try{
-        const profile = await Profile.findOne({ user: req.user.id });
+    try{
+      const profile = await Profile.findOne({ user: req.user.id });
 
-        profile.experience.unshift(newExp);
+      profile.experience.unshift(newExp);
 
-        res.json(profile);
-      }catch(err){
-        console.error(err.message);
-        res.status(500).send('Server Error');
-      }
+      await profile.save();
+
+      res.json(profile);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
